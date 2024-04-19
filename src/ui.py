7caeffe7d6cadca6346b62ab6b2658d203a72f52
TanishSinghTak <tanishsingh.tak9829@gmail.com>
2024-04-19 18:20:44 +0530

    some final changes

--- src/ui.py
+++ src/ui.py
@@ -39,16 +39,6 @@ class UI:
 		pygame.draw.rect(self.display_surface,color,current_rect)
 		pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
 
-	def show_exp(self,exp):
-		text_surf = self.font.render(str(int(exp)),False,TEXT_COLOR)
-		x = self.display_surface.get_size()[0] - 20
-		y = self.display_surface.get_size()[1] - 20
-		text_rect = text_surf.get_rect(bottomright = (x,y))
-
-		pygame.draw.rect(self.display_surface,UI_BG_COLOR,text_rect.inflate(20,20))
-		self.display_surface.blit(text_surf,text_rect)
-		pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,text_rect.inflate(20,20),3)
-
 	def selection_box(self,left,top, has_switched):
 		bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
 		pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
@@ -76,8 +66,6 @@ class UI:
 
 	def display(self,player):
 		self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
-
-		self.show_exp(player.exp)
 
 		self.weapon_overlay(player.weapon_index,not player.can_switch_weapon)
 
